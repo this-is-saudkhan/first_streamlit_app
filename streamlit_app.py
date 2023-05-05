db1d2997e0856d4431b3bc79ed1b82d19f9fd266
this-is-saudkhan <33393043+this-is-saudkhan@users.noreply.github.com>
2023-05-05 11:31:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
+    return streamlit.dataframe(fruityvice_normalized)
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   
@@ -23,11 +28,14 @@ try:
   if not fruit_choice:
     streamlit.error("Plz select a fruit")
   else
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalised= pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalised)
+    back_from_funct=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+  
+  
 except URLError as e:
   streamlit.error()
+  
+  
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
